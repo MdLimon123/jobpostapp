98ab01a320bb78191a8a23516852d58bd8aa713d
Limon <limon222020@gmail.com>
2023-09-12 10:55:22 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/limon/job_post_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.limon.job_post_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('Job Post app is being initialization ',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: width * 0.045,
+                    fontWeight: FontWeight.bold
+                  ),),
+                ),
+              ),
+            );
+          }else if(snapshot.hasError){
+
+            MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('An error has been occurred ',
+                    style: TextStyle(
+                        color: Colors.cyan,
+                        fontSize: width * 0.045,
+                        fontWeight: FontWeight.bold
+                    ),),
+                ),
+              ),
+            );
+
+          }
+          return MaterialApp(
+            title: "Job Post App",
+            theme: ThemeData(
+              scaffoldBackgroundColor: Colors.black,
+              primaryColor: Colors.blue
+            ),
+            home: Scaffold(),
+          );
+        });
+  }
+}
+
+
