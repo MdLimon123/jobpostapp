d908e8802b29decba06aa3b63dd064ff36c27c4b
Limon <limon222020@gmail.com>
2023-09-19 11:33:07 -0700

    logout done

---
+++ lib/Jobs/upload_job.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../Widgets/bottom_nav_bar.dart';
+
+class UploadJobNow extends StatefulWidget {
+
+  const UploadJobNow({super.key});
+
+  @override
+  State<UploadJobNow> createState() => _UploadJobNowState();
+}
+
+class _UploadJobNowState extends State<UploadJobNow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Colors.deepOrange.shade300,
+                Colors.blueAccent
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              stops: const [0.2, 0.9]
+          )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+       bottomNavigationBar: BottomNavigationBarForApp(indexNum: 2),
+        appBar: AppBar(
+          title: const Text('Upload Job Now'),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    stops: const [0.2, 0.9]
+                )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Widgets/bottom_nav_bar.dart
+++ lib/Widgets/bottom_nav_bar.dart
@@ -1,13 +1,64 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:job_post_app/Jobs/jobs_screen.dart';
+import 'package:job_post_app/Jobs/upload_job.dart';
+import 'package:job_post_app/Search/profile_company.dart';
 import 'package:job_post_app/Search/search_companies.dart';
+import 'package:job_post_app/user_state.dart';
 
 class BottomNavigationBarForApp extends StatelessWidget {
 
   BottomNavigationBarForApp({required this.indexNum});
 
  int indexNum = 0;
+
+ void _logout(context){
+   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+   showDialog(
+       context: context,
+       builder: (context){
+         return  AlertDialog(
+           backgroundColor: Colors.black54,
+           title: const Row(
+             children: [
+               Padding(padding: EdgeInsets.all(8.0),
+                 child: Icon(Icons.logout,
+                 color: Colors.white,
+                     size: 36),
+               ),
+               Padding(padding: EdgeInsets.all(8.0),
+               child: Text('Sign Out',
+               style: TextStyle(color: Colors.white,
+               fontSize: 28),),)
+
+             ],
+           ),
+           content: const Text('Do you want to Log Out?',
+           style: TextStyle(
+             fontSize: 20,
+             color: Colors.white
+           ),),
+           actions: [
+             TextButton(onPressed: (){
+               Navigator.canPop(context)?Navigator.pop(context) : null;
+             },
+                 child: const Text('No',
+                 style: TextStyle(color: Colors.green,
+                 fontSize: 18),)),
+             TextButton(onPressed: (){
+               _auth.signOut();
+               Navigator.canPop(context)?Navigator.pop(context) : null;
+               Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> UserState()));
+             },
+                 child: const Text('Yes',
+                   style: TextStyle(color: Colors.green,
+                       fontSize: 18),)),
+           ],
+         );
+       });
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +76,26 @@ class BottomNavigationBarForApp extends StatelessWidget {
         ),
         items:[
           Icon(Icons.list, size: width * 0.060, color: Colors.black,),
-          Icon(Icons.search, size: width * 0.060, color: Colors.black,)
+          Icon(Icons.search, size: width * 0.060, color: Colors.black,),
+          Icon(Icons.add, size: width * 0.060, color: Colors.black,),
+          Icon(Icons.person_pin, size: width * 0.060, color: Colors.black,),
+          Icon(Icons.exit_to_app, size: width * 0.060, color: Colors.black,)
         ] ,
       onTap: (index){
         if(index == 0){
           Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> JobsScreen()));
-        }else if(index ==1){
+        }
+        else if(index ==1){
           Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> AllWorkersScreen()));
+        }
+        else if(index ==2){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> UploadJobNow()));
+        }
+        else if(index ==3){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=> ProfileScreen()));
+        }
+        else if(index ==4){
+          _logout(context);
         }
       },
     );
